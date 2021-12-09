fc47454e95398126c0559cc7bb78e48e34dea70f
Abiel Sandoval <20197806@itla.edu.do>
2021-12-09 15:58:13 -0400

    Cambio realizado en el NavBar

--- biblioteca/obj/Debug/net5.0/RazorDeclaration/Pages/FiltroLibrosVentaAlta.razor.g.cs
+++ biblioteca/obj/Debug/net5.0/RazorDeclaration/Pages/FiltroLibrosVentaAlta.razor.g.cs
@@ -13,84 +13,84 @@ namespace biblioteca.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 1 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 2 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 3 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 4 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 5 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 6 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 7 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 8 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 9 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using biblioteca;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 10 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using biblioteca.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\_Imports.razor"
+#line 12 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\_Imports.razor"
 using biblioteca.Data.AdminUsuarios;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\Pages\FiltroLibrosVentaAlta.razor"
+#line 2 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\Pages\FiltroLibrosVentaAlta.razor"
 using biblioteca.Models;
 
 #line default
@@ -105,7 +105,7 @@ using biblioteca.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 34 "C:\Users\alane\OneDrive\Documentos\Clases Semestre 7\Ingenieria de Software\Proyecto Final\Proyecto_Biblioteca-main\Proyecto_Biblioteca-main\biblioteca\Pages\FiltroLibrosVentaAlta.razor"
+#line 34 "C:\Users\HP 5540\Desktop\Introduccion a la Ingenieria de Software\Biblioteca\Proyecto_Biblioteca\biblioteca\Pages\FiltroLibrosVentaAlta.razor"
        
     List<Libros> GetLibros() => new btnnoysoy5udemk46n23Context().Libros.OrderByDescending(l => l.Ventas).ToList();
 
